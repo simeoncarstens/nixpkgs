0c4040b3649ba5dd53ec09abb32de6aace2feba9
Simeon Carstens <simeon.carstens@tweag.io>
2023-01-14 22:30:30 +0100

    More WIP

--- pkgs/misc/bridgestan/default.nix
+++ pkgs/misc/bridgestan/default.nix
@@ -3,29 +3,30 @@ stdenv.mkDerivation {
   pname = "bridgestan";
   version = "1.0.0";
   # src = fetchFromGitHub {
-  #   owner = "roualdes";
-  #   repo = "bridgestan";
-  #   rev = "v1.0.0";
-  #   sha256 = "19yh58qyds751yzgrkqk66wd63bg11xnf2zacrqz0xr0nr507y7k";
-  #   fetchSubmodules = true;
-  # };
-  src = fetchgit {
-    url = "https://github.com/roualdes/bridgestan.git";
-    rev = "v1.0.0";
-    sha256 = "pAn0bs+u5aS61kZy6LAsUzB6OUz1WOKt7f4EVV3FIHY=";
-    fetchSubmodules = true;
-  };
-  patchPhase = ''
-    substituteInPlace Makefile --replace 'STANC ?= $(BS_ROOT)/bin/stanc$(EXE)' 'STANC ?= ${stanc}/bin/stanc'   
-    head -n -10 Makefile > Makefile.tmp && mv Makefile.tmp Makefile
+    #   owner = "roualdes";
+    #   repo = "bridgestan";
+    #   rev = "v1.0.0";
+    #   sha256 = "19yh58qyds751yzgrkqk66wd63bg11xnf2zacrqz0xr0nr507y7k";
+    #   fetchSubmodules = true;
+    # };
+    src = fetchgit {
+      url = "https://github.com/roualdes/bridgestan.git";
+      rev = "v1.0.0";
+      sha256 = "pAn0bs+u5aS61kZy6LAsUzB6OUz1WOKt7f4EVV3FIHY=";
+      fetchSubmodules = true;
+    };
+    patchPhase = ''
+      substituteInPlace Makefile --replace 'STANC ?= $(BS_ROOT)/bin/stanc$(EXE)' 'STANC ?= ${stanc}/bin/stanc'   
+      head -n -10 Makefile > Makefile.tmp && mv Makefile.tmp Makefile
     '';
-  installPhase = ''
-    mkdir $out
-    cp -r $src/* $out/
-    ls -alh $out/
-    cat <<EOT >> $out/bin/bridgestan_make
-    #!/usr/bin/env bash
-    ${gnumake}/bin/make $@
-    EOT 
-  '';
+    installPhase = ''
+      mkdir -p $out/bin
+      cp -r $src/* $out/
+      # cat <<EOT >> $out/bin/bridgestan_make
+      # #!/usr/bin/env bash
+      # cd $out
+      # ${gnumake}/bin/make $@
+      # EOT
+      # chmod +x $out/bin/bridgestan_make
+    '';
 }
